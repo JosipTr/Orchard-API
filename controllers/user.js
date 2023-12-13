d9c56d453aa8b3786f0f13d29b9029384ddbd105
JosipTr <jtr.other13@gmail.com>
2023-12-13 13:23:00 +0100

    Change response

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,8 @@ const User = require("../models/user");
 exports.getUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.userId);
-    return res.status(200).json({ user: user });
+    const email = user.email;
+    return res.status(200).json({ email: email });
   } catch (err) {
     next(err);
   }
