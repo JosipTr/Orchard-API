9a433719921fc892076a14ed76519053ff9fc5d2
JosipTr <jtr.other13@gmail.com>
2023-12-16 11:02:51 +0100

    Fix getCart

--- controllers/cart.js
+++ controllers/cart.js
@@ -3,7 +3,17 @@ const Cart = require("../models/cart");
 exports.getCart = async (req, res, next) => {
   try {
     const cart = await Cart.findOne({ user: req.userId });
+    if (!cart) {
+      const cartTemp = new Cart({
+        products: [],
+        user: req.userId,
+      });
+      await cartTemp.save();
+      res.status(200).json({ cart: cartTemp });
+      return;
+    }
     res.status(200).json({ cart: cart });
+    return;
   } catch (err) {
     next(err);
   }
